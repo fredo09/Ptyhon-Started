610fd4553c0058a742a543a09e45c4ff33134747
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2019-11-26 13:20:43 -0600

    Conceptos basicos Python

---
+++ Listas/listas.py
@@ -0,0 +1,15 @@
+""" Listas en python """
+
+milista = []
+milista.append(1)
+milista.append(2)
+milista.append(3)
+
+print (milista[0])
+print (milista[1])
+print (milista[2])
+
+# recorriendo la lista con un for
+for x in milista:
+    print("Empezando a recorrer la lista")
+    print (x)
---
+++ variables-tipos/variables_tipos.py
@@ -0,0 +1,26 @@
+# Variables Numeros
+myint = 7
+
+""" 
+Para definir un numero flotante, se de
+debe usar la siguiente notacion.
+"""
+midecimal = 7.0
+midecimal = float(7)
+
+# Cadenas Ejemplos
+
+micadena = 'Hola'
+micadena = "Hola"
+
+# Operaciones 
+uno = 1
+dos = 2
+tres = uno + dos
+print(tres)
+
+hola = "Hola"
+mundo = "mundo"
+holamundo = hola + " " + mundo
+print(holamundo)
+
