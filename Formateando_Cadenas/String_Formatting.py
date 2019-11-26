398dcf84ce2af249c3411618f323a97b9487b1f0
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2019-11-26 16:10:28 -0600

    Agregando Nuevos Conceptos

---
+++ Formateando_Cadenas/String_Formatting.py
@@ -0,0 +1,7 @@
+#Esto imprime "Hola, Fredy!"
+nombre = "Fredy"
+print ("Hola, %s!" % nombre)
+
+# Esto muestra lo siguiente: Una Lista: [1, 2, 3]
+milista = [1,2,3]
+print ("Una lista: %s" % milista)
